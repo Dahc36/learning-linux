9953601ced80460e9160ffd28e84e59180f1e13c
David Hans <david@havenservicing.com>
2021-12-28 21:43:40 -0300

    ADD: websetup with args

---
+++ scripts/web_template_setup.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# vars
+# TEMPLATE_DIR="2058_metallic_slider" TODO: default
+TEMPLATE_DIR=$1
+TEMPLATE_URL="https://www.tooplate.com/zip-templates/$TEMPLATE_DIR.zip"
+TEMP_DIR="/tmp/webfiles"
+SERVER_DIR="/var/www/html"
+
+echo "Installing dependencies..."
+sudo apt-get update > /dev/null
+sudo apt-get install apache2 > /dev/null
+
+echo "Downloading template..."
+mkdir -p $TEMP_DIR
+cd $TEMP_DIR
+sudo wget --output-file /dev/null $TEMPLATE_URL
+sudo unzip $TEMPLATE_DIR.zip > /dev/null
+
+echo "Moving template to server source..."
+sudo rm -rf $SERVER_DIR
+sudo mkdir $SERVER_DIR
+sudo cp -r $TEMPLATE_DIR/* $SERVER_DIR
+
+echo "Restarting server..."
+service apache2 reload
+
+echo "Cleaning up..."
+sudo rm -rf $TEMP_DIR
