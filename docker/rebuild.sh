56ec03ad96949849e9a97f5f02029d5ceeba56ad
David Hans <david@havenservicing.com>
2022-01-07 03:04:21 -0300

    ADD: docker-compose

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,11 +1,20 @@
 #!/bin/zsh
 
-TAG_NAME=$1
-PORT=$2
+ROUTE=$1
+TAG_NAME=$2
+PORT=$3
+
+if [[ -z $ROUTE ]]; then
+  echo "Route not provided"
+  exit
+fi
 
 if [[ -z $TAG_NAME ]]; then
   TAG_NAME="fashion"
-  PORT='3000'
+fi
+
+if [[ -z $PORT ]]; then
+  PORT="3000"
 fi
 
 
@@ -15,7 +24,7 @@ docker rm $TAG_NAME
 docker rmi $TAG_NAME
 
 echo "Building new stuff..."
-docker build -t $TAG_NAME .
+docker build -t $TAG_NAME $ROUTE
 docker run -d --name $TAG_NAME -p $PORT:80 $TAG_NAME
 
 docker ps
